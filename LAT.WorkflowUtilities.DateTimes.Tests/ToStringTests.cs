a2a3a2e591687412e5fd422abe77b3ba9832bf0d
Steve Platz <steve_platz@lord.com>
2017-09-07 20:48:25 -0400

    Add ToString workflow action

---
+++ LAT.WorkflowUtilities.DateTimes.Tests/ToStringTests.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using Moq;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+
+namespace LAT.WorkflowUtilities.DateTimes.Tests
+{
+    [TestClass]
+    public class ToStringTests
+    {
+        #region Class Constructor
+        private readonly string _namespaceClassAssembly;
+        public ToStringTests()
+        {
+            //[Namespace.class name, assembly name] for the class/assembly being tested
+            //Namespace and class name can be found on the class file being tested
+            //Assembly name can be found under the project properties on the Application tab
+            _namespaceClassAssembly = "LAT.WorkflowUtilities.DateTimes.ToString" + ", " + "LAT.WorkflowUtilities.DateTimes";
+        }
+        #endregion
+        #region Test Initialization and Cleanup
+        // Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void ClassInitialize(TestContext testContext) { }
+
+        // Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void ClassCleanup() { }
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void TestMethodInitialize() { }
+
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void TestMethodCleanup() { }
+        #endregion
+
+        [TestMethod]
+        public void Date_Default()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object> 
+            {
+                { "DateToUse", new DateTime(2014, 12, 31, 23, 0, 0, 0)},
+                { "EvaluateAsUserLocal", true},
+                { "Culture", "en-US" },
+                { "Format", "g" }
+            };
+
+            //Expected value
+            const string expected = "12/31/2014 11:00 PM";
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        [TestMethod]
+        public void Date_esES()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object>
+            {
+                { "DateToUse", new DateTime(2014, 12, 31, 23, 0, 0, 0)},
+                { "EvaluateAsUserLocal", true},
+                { "Culture", "es-ES" },
+                { "Format", "g" }
+            };
+
+            //Expected value
+            const string expected = "31/12/2014 23:00";
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        [TestMethod]
+        public void Date_InvalidCulture()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object> 
+            {
+                { "DateToUse", new DateTime(2014, 12, 31, 23, 0, 0, 0)},
+                { "EvaluateAsUserLocal", true},
+                { "Culture", "xx5x" },
+                { "Format", "g" }
+            };
+
+            //Expected value
+            const string expected = null;
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        [TestMethod]
+        public void Date_InvalidFormat()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object>
+            {
+                { "DateToUse", new DateTime(2014, 12, 31, 23, 0, 0, 0)},
+                { "EvaluateAsUserLocal", true},
+                { "Culture", "en-US" },
+                { "Format", "ZZZ" }
+            };
+
+            //Expected value
+            const string expected = "ZZZ";
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        [TestMethod]
+        public void Date_ValidDateUtc()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object>
+            {
+                { "DateToUse", new DateTime(2015, 1, 1, 5, 0, 0, 0, DateTimeKind.Utc)},
+                { "EvaluateAsUserLocal", false},
+                { "Culture", "en-US" },
+                { "Format", "g" }
+            };
+
+            //Expected value
+            const string expected = "1/1/2015 5:00 AM";
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        [TestMethod]
+        public void Date_ValidDateUserLocal()
+        {
+            //Target
+            Entity targetEntity = null;
+
+            //Input parameters
+            var inputs = new Dictionary<string, object>
+            {
+                { "DateToUse", new DateTime(2015, 1, 1, 5, 0, 0, 0, DateTimeKind.Utc)},
+                { "EvaluateAsUserLocal", true},
+                { "Culture", "en-US" },
+                { "Format", "g" }
+            };
+
+            //Expected value
+            const string expected = "12/31/2014 11:00 PM";
+
+            //Invoke the workflow
+            var output = InvokeWorkflow(_namespaceClassAssembly, ref targetEntity, inputs, ValidDayUserLocalSetup);
+
+            //Test
+            Assert.AreEqual(expected, output["FormattedDateString"]);
+        }
+
+        /// <summary>
+        /// Invokes the workflow.
+        /// </summary>
+        /// <param name="name">Namespace.Class, Assembly</param>
+        /// <param name="target">The target entity</param>
+        /// <param name="inputs">The workflow input parameters</param>
+        /// <param name="configuredServiceMock">The function to configure the Organization Service</param>
+        /// <returns>The workflow output parameters</returns>
+        private static IDictionary<string, object> InvokeWorkflow(string name, ref Entity target, Dictionary<string, object> inputs,
+            Func<Mock<IOrganizationService>, Mock<IOrganizationService>> configuredServiceMock)
+        {
+            var testClass = Activator.CreateInstance(Type.GetType(name)) as CodeActivity; ;
+
+            var serviceMock = new Mock<IOrganizationService>();
+            var factoryMock = new Mock<IOrganizationServiceFactory>();
+            var tracingServiceMock = new Mock<ITracingService>();
+            var workflowContextMock = new Mock<IWorkflowContext>();
+
+            //Apply configured Organization Service Mock
+            if (configuredServiceMock != null)
+                serviceMock = configuredServiceMock(serviceMock);
+
+            IOrganizationService service = serviceMock.Object;
+
+            //Mock workflow Context
+            var workflowUserId = Guid.NewGuid();
+            var workflowCorrelationId = Guid.NewGuid();
+            var workflowInitiatingUserId = Guid.NewGuid();
+
+            //Workflow Context Mock
+            workflowContextMock.Setup(t => t.InitiatingUserId).Returns(workflowInitiatingUserId);
+            workflowContextMock.Setup(t => t.CorrelationId).Returns(workflowCorrelationId);
+            workflowContextMock.Setup(t => t.UserId).Returns(workflowUserId);
+            var workflowContext = workflowContextMock.Object;
+
+            //Organization Service Factory Mock
+            factoryMock.Setup(t => t.CreateOrganizationService(It.IsAny<Guid>())).Returns(service);
+            var factory = factoryMock.Object;
+
+            //Tracing Service - Content written appears in output
+            tracingServiceMock.Setup(t => t.Trace(It.IsAny<string>(), It.IsAny<object[]>())).Callback<string, object[]>(MoqExtensions.WriteTrace);
+            var tracingService = tracingServiceMock.Object;
+
+            //Parameter Collection
+            ParameterCollection inputParameters = new ParameterCollection { { "Target", target } };
+            workflowContextMock.Setup(t => t.InputParameters).Returns(inputParameters);
+
+            //Workflow Invoker
+            var invoker = new WorkflowInvoker(testClass);
+            invoker.Extensions.Add(() => tracingService);
+            invoker.Extensions.Add(() => workflowContext);
+            invoker.Extensions.Add(() => factory);
+
+            return invoker.Invoke(inputs);
+        }
+
+        /// <summary>
+        /// Modify to mock CRM Organization Service actions
+        /// </summary>
+        /// <param name="serviceMock">The Organization Service to mock</param>
+        /// <returns>Configured Organization Service</returns>
+        private static Mock<IOrganizationService> ValidDayUserLocalSetup(Mock<IOrganizationService> serviceMock)
+        {
+            EntityCollection userSettings = new EntityCollection();
+            Entity userSetting = new Entity("usersettings");
+            userSetting["timezonecode"] = 20;
+            userSettings.Entities.Add(userSetting);
+
+            serviceMock.Setup(t =>
+                t.RetrieveMultiple(It.IsAny<QueryExpression>()))
+                .ReturnsInOrder(userSettings);
+
+            OrganizationResponse localTime = new OrganizationResponse();
+            ParameterCollection results = new ParameterCollection { { "LocalTime", new DateTime(2014, 12, 31, 23, 0, 0) } };
+            localTime.Results = results;
+
+            serviceMock.Setup(t =>
+                t.Execute(It.IsAny<OrganizationRequest>()))
+                .ReturnsInOrder(localTime);
+
+            return serviceMock;
+        }
+    }
+}
---
+++ LAT.WorkflowUtilities.DateTimes/ToString.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Globalization;
+using LAT.WorkflowUtilities.DateTimes.Common;
+
+namespace LAT.WorkflowUtilities.DateTimes
+{
+    public sealed class ToString : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Date To Use")]
+        public InArgument<DateTime> DateToUse { get; set; }
+
+        [RequiredArgument]
+        [Input("Evaluate As User Local")]
+        [Default("True")]
+        public InArgument<bool> EvaluateAsUserLocal { get; set; }
+
+        [Input("Culture")]
+        [Default("en-US")]
+        public InArgument<string> Culture { get; set; }
+
+        [Input("Format")]
+        [Default("g")]
+        public InArgument<string> Format { get; set; }
+
+        [OutputAttribute("Formatted Date String")]
+        public OutArgument<string> FormattedDateString { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                DateTime dateToUse = DateToUse.Get(executionContext);
+                bool evaluateAsUserLocal = EvaluateAsUserLocal.Get(executionContext);
+                string cultureIn = Culture.Get(executionContext);
+                string format = Format.Get(executionContext);
+
+                CultureInfo culture = null;
+                if (!string.IsNullOrEmpty(cultureIn))
+                    culture = new CultureInfo(cultureIn);
+
+                if (evaluateAsUserLocal)
+                {
+                    GetLocalTime glt = new GetLocalTime();
+                    int? timeZoneCode = glt.RetrieveTimeZoneCode(service);
+                    dateToUse = glt.RetrieveLocalTimeFromUtcTime(dateToUse, timeZoneCode, service);
+                }
+
+                string formattedDateString = dateToUse.ToString(format, culture);
+
+                FormattedDateString.Set(executionContext, formattedDateString);
+            }
+            catch (Exception ex)
+            {
+                tracer.Trace("Exception: {0}", ex.ToString());
+            }
+        }
+    }
+}
